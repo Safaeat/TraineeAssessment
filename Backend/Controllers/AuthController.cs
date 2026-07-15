f32857aee8d7d071dd3cbdb6da610aeef796acfb
Safaeat <safaeat.11.sma@gmail.com>
2026-07-16 03:18:40 +0600

    Login succes and Authentication

--- Backend/Controllers/AuthController.cs
+++ Backend/Controllers/AuthController.cs
@@ -1,4 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Backend.DTOs;
+using Backend.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace Backend.Controllers
 {
@@ -6,6 +12,76 @@ namespace Backend.Controllers
     [Route("api/[controller]")]
     public class AuthController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
 
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginRequest request)
+        {
+            if (string.IsNullOrWhiteSpace(request.Username) ||
+                string.IsNullOrWhiteSpace(request.Password))
+            {
+                return BadRequest(new LoginResponse
+                {
+                    Success = false,
+                    Message = "Username and Password are required."
+                });
+            }
+
+            var users = _configuration
+                        .GetSection("Users")
+                        .Get<List<User>>();
+
+            var user = users?.FirstOrDefault(u =>
+                        u.Username == request.Username &&
+                        u.Password == request.Password);
+
+            if (user == null)
+            {
+                return Unauthorized(new LoginResponse
+                {
+                    Success = false,
+                    Message = "Invalid Username or Password."
+                });
+            }
+
+            var token = GenerateJwtToken(user.Username);
+
+            return Ok(new LoginResponse
+            {
+                Success = true,
+                Username = user.Username,
+                Token = token,
+                Message = "Login successful"
+            });
+        }
+
+        private string GenerateJwtToken(string username)
+        {
+            var claims = new[]
+            {
+        new Claim(ClaimTypes.Name, username)
+    };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
+
+            var credentials = new SigningCredentials(
+                key,
+                SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(2),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
     }
 }
